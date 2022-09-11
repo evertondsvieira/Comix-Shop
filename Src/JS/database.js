d55248abbc1523fd7d13f240aad33df3295dac5e
Everton Vieira <vieiraeverton33@hotmail.com>
2022-09-10 21:30:23 -0300

    feat: created database

---
+++ Src/JS/database.js
@@ -0,0 +1,52 @@
+const carrinho = []
+
+const data = [
+    {
+        id: 1,
+        img: "../IMG/imagem-01",
+        nameItem: "Berserk",
+        description: "Guts é um ex-mercenário espadach...",
+        value: 39.90,
+        tag: "Mangá",
+    },
+    {
+        id: 2,
+        img: "../IMG/imagem-02",
+        nameItem: "Bleach",
+        description: "Guts é um ex-mercenário espadach...",
+        value: 29.90,
+        tag: "Mangá",
+    },
+    {
+        id: 3,
+        img: "../IMG/imagem-03",
+        nameItem: "Naruto",
+        description: "asdasd",
+        value: 29.90,
+        tag: "Mangá",
+    },
+    {
+        id: 4,
+        img: "../IMG/imagem-04",
+        nameItem: "JoJo Bizarre Adventure",
+        description: "asdasd",
+        value: 39.90,
+        tag: "Mangá",
+    },
+    {
+        id: 5,
+        img: "../IMG/imagem-05",
+        nameItem: "Amazing Spider-Man",
+        description: "asdasd",
+        value: 29.90,
+        tag: "HQ",
+    },
+    {
+        id: 6,
+        img: "../IMG/imagem-06",
+        nameItem: "Turma da Mônica",
+        description: "asdasd",
+        value: 9.90,
+        tag: "Gibi",
+    },
+]
