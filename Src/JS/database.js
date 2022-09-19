3c4a170ecfd34e19a71b337a2ac0702f1206893e
Everton Vieira <vieiraeverton33@hotmail.com>
2022-09-18 21:27:00 -0300

    feat: added news works

--- Src/JS/database.js
+++ Src/JS/database.js
@@ -3,50 +3,92 @@ const carrinho = []
 const data = [
     {
         id: 1,
-        img: "../IMG/imagem-01",
+        img: "https://img1.ak.crunchyroll.com/i/spire1/3fc5239256fd261e25285cd5499125571649079550_main.jpg",
         nameItem: "Berserk",
         description: "Guts é um ex-mercenário espadach...",
         value: 39.90,
         tag: "Mangá",
+        addCart: "Adicionar ao carrinho",
+        removeCart: "Remover produto"
     },
     {
         id: 2,
-        img: "../IMG/imagem-02",
-        nameItem: "Bleach",
-        description: "Guts é um ex-mercenário espadach...",
+        img: "https://images-na.ssl-images-amazon.com/images/I/51UwYyZvCDL.jpg",
+        nameItem: "Viland Saga",
+        description: "Quando Thors é convocado para mais uma batalha...",
         value: 29.90,
         tag: "Mangá",
+        addCart: "Adicionar ao carrinho",
+        removeCart: "Remover produto"
     },
     {
         id: 3,
-        img: "../IMG/imagem-03",
-        nameItem: "Naruto",
-        description: "asdasd",
+        img: "https://comicvine.gamespot.com/a/uploads/scale_small/6/67663/2720271-31.jpg",
+        nameItem: "Hanma Baki",
+        description: "Decidido a conquistar o título do seu pai...",
         value: 29.90,
         tag: "Mangá",
+        addCart: "Adicionar ao carrinho",
+        removeCart: "Remover produto"
     },
     {
         id: 4,
-        img: "../IMG/imagem-04",
-        nameItem: "JoJo Bizarre Adventure",
-        description: "asdasd",
+        img: "https://i.ebayimg.com/images/g/toQAAOSwIspaYF3Z/s-l500.jpg",
+        nameItem: "JBA Steel Ball Run",
+        description: "Gyro Zeppeli, um executor do reino de Neapolis...",
         value: 39.90,
         tag: "Mangá",
+        addCart: "Adicionar ao carrinho",
+        removeCart: "Remover produto"
     },
     {
         id: 5,
-        img: "../IMG/imagem-05",
-        nameItem: "Amazing Spider-Man",
-        description: "asdasd",
+        img: "https://i.pinimg.com/originals/08/79/51/08795135956735263da8b9c8495ae667.jpg",
+        nameItem: "Initial D",
+        description: "Ambientado na província de Gunma durante os anos 90...",
         value: 29.90,
-        tag: "HQ",
+        tag: "Mangá",
+        addCart: "Adicionar ao carrinho",
+        removeCart: "Remover produto"
     },
     {
         id: 6,
-        img: "../IMG/imagem-06",
+        img: "https://i.ibb.co/gMr8h1V/imagem-05.jpg",
+        nameItem: "The Amazing Spider-Man",
+        description: "The Amazing Spider-Man é uma série de quadrinhos...",
+        value: 29.90,
+        tag: "HQ",
+        addCart: "Adicionar ao carrinho",
+        removeCart: "Remover produto"
+    },
+    {
+        id: 7,
+        img: "https://www.mestrejedi.com.br/wp-content/uploads/2019/02/star-wars-108-marvel-comics-variant-cover-1158313.jpeg",
+        nameItem: "Star Wars",
+        description: "A princesa Leia é mantida refém pelas forças imperial...",
+        value: 49.90,
+        tag: "HQ",
+        addCart: "Adicionar ao carrinho",
+        removeCart: "Remover produto"
+    },
+    {
+        id: 8,
+        img: "https://i.ibb.co/XsX2tpG/imagem-06.jpg",
         nameItem: "Turma da Mônica",
-        description: "asdasd",
+        description: "Turma da Mônica é uma série de histórias em quadrinhos...",
         value: 9.90,
         tag: "Gibi",
+        addCart: "Adicionar ao carrinho",
+        removeCart: "Remover produto"
+    },
+    {
+        id: 9,
+        img: "https://dvygmnfdru1l.cloudfront.net/V52JHrOLo4tyj6KAk1UE/162/3376782d-50b4-4908-a7c4-363bbe850575",
+        nameItem: "Cebolinha",
+        description: "Cebolinha é um personagem de histórias em quadrinhos...",
+        value: 9.90,
+        tag: "Gibi",
+        addCart: "Adicionar ao carrinho",
+        removeCart: "Remover produto"
     },
 ]
