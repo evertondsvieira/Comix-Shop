07b35b10a8ec122f5b7552591ac946c5ed120397
Everton Vieira <vieiraeverton33@hotmail.com>
2022-09-23 20:50:19 -0300

    fix: fixed imgs link

--- Src/JS/database.js
+++ Src/JS/database.js
@@ -3,7 +3,7 @@ let originalCart = []
 const data = [
     {
         id: 1,
-        img: "https://img1.ak.crunchyroll.com/i/spire1/3fc5239256fd261e25285cd5499125571649079550_main.jpg",
+        img: "https://i.ibb.co/cyPwzCH/berserk.jpg",
         nameItem: "Berserk",
         description: "Guts é um ex-mercenário e integrante do Bando do Fal...",
         value: 39.90,
@@ -13,7 +13,7 @@ const data = [
     },
     {
         id: 2,
-        img: "https://images-na.ssl-images-amazon.com/images/I/51UwYyZvCDL.jpg",
+        img: "https://i.ibb.co/FgcM5b3/51-Uw-Yy-Zv-CDL.jpg",
         nameItem: "Viland Saga",
         description: "Quando Thors é convocado para mais uma batalha...",
         value: 29.90,
@@ -23,7 +23,7 @@ const data = [
     },
     {
         id: 3,
-        img: "https://comicvine.gamespot.com/a/uploads/scale_small/6/67663/2720271-31.jpg",
+        img: "https://i.ibb.co/f4pdNxD/2720271-31.jpg",
         nameItem: "Hanma Baki",
         description: "Decidido a conquistar o título do seu pai...",
         value: 29.90,
@@ -33,7 +33,7 @@ const data = [
     },
     {
         id: 4,
-        img: "https://i.ebayimg.com/images/g/toQAAOSwIspaYF3Z/s-l500.jpg",
+        img: "https://i.ibb.co/P1NTkVK/s-l500.jpg",
         nameItem: "JJBA Steel Ball Run",
         description: "Gyro Zeppeli, um executor do reino de Neapolis...",
         value: 39.90,
@@ -43,7 +43,7 @@ const data = [
     },
     {
         id: 5,
-        img: "https://i.pinimg.com/originals/08/79/51/08795135956735263da8b9c8495ae667.jpg",
+        img: "https://i.ibb.co/J7fwh4S/08795135956735263da8b9c8495ae667.jpg",
         nameItem: "Initial D",
         description: "Ambientado na província de Gunma durante os anos 90...",
         value: 29.90,
@@ -53,7 +53,7 @@ const data = [
     },
     {
         id: 6,
-        img: "https://comicvine.gamespot.com/a/uploads/scale_small/6/67663/6428286-50.jpg",
+        img: "https://i.ibb.co/0nCB2d1/6428286-50.jpg",
         nameItem: "Kingdom",
         description: "A história de Kingdom é uma adaptação fictícia de um...",
         value: 39.90,
@@ -63,7 +63,7 @@ const data = [
     },
     {
         id: 7,
-        img: "https://i.ibb.co/gMr8h1V/imagem-05.jpg",
+        img: "https://i.ibb.co/c19pH0z/imagem-05.jpg",
         nameItem: "Spider-Man",
         description: "The Amazing Spider-Man é uma série de quadrinhos...",
         value: 29.90,
@@ -73,7 +73,7 @@ const data = [
     },
     {
         id: 8,
-        img: "https://www.mestrejedi.com.br/wp-content/uploads/2019/02/star-wars-108-marvel-comics-variant-cover-1158313.jpeg",
+        img:"https://i.ibb.co/DL9BK2h/star-wars-108-marvel-comics-variant-cover-1158313.jpg",
         nameItem: "Star Wars",
         description: "A princesa Leia é mantida refém pelas forças imperial...",
         value: 49.90,
@@ -83,7 +83,7 @@ const data = [
     },
     {
         id: 9,
-        img: "https://hqrock.files.wordpress.com/2011/08/themanwholaughs.jpg",
+        img: "https://i.ibb.co/J5YC657/themanwholaughs.jpg",
         nameItem: "Batman",
         description: "História do primeiro confronto entre o Batman e o Coringa...",
         value: 29.90,
@@ -93,7 +93,7 @@ const data = [
     },
     {
         id: 10,
-        img: "https://a-static.mlcdn.com.br/1500x1500/gibi-tio-patinhas-disney-unidade-hq-cultural/klebervariedades/15788319793/634d5abcb16ce03c59f99a24a2d6b37f.jpg",
+        img: "https://i.ibb.co/CP6Mvhy/634d5abcb16ce03c59f99a24a2d6b37f.webp",
         nameItem: "Tio Patinhas",
         description: "Tio Patinhas é conhecido como o pato mais rico do mundo...",
         value: 19.90,
@@ -103,7 +103,7 @@ const data = [
     },
     {
         id: 11,
-        img: "https://i.ibb.co/XsX2tpG/imagem-06.jpg",
+        img: "https://i.ibb.co/VCNL5Hn/imagem-06.jpg",
         nameItem: "Mônica",
         description: "Turma da Mônica é uma série de histórias em quadrinhos...",
         value: 9.90,
@@ -113,7 +113,7 @@ const data = [
     },
     {
         id: 12,
-        img: "https://dreambook.com.br/media/catalog/product/cache/3d487d6ecb524ce0058195848e1306a2/9/7/9788539418817.jpg",
+        img: "https://i.ibb.co/89HWFvD/9788539418817.jpg",
         nameItem: "Cebolinha",
         description: "Cebolinha é um personagem de histórias em quadrinhos...",
         value: 9.90,
